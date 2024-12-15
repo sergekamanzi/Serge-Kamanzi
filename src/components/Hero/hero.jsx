7cd35d746a7b7840ae3b60a55aec774374ceeced
sergekamanzi <s.kamanzi@alustudent.com>
2024-12-15 21:21:50 +0200

    serge.k

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./hero.css";
 import { IoMdDownload } from "react-icons/io";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
@@ -6,6 +6,15 @@ import { RiInstagramFill } from "react-icons/ri";
 import { FaXTwitter } from "react-icons/fa6";
 
 const Hero = () => {
+  const [showResume, setShowResume] = useState(true);
+
+  const socialMediaLinks = {
+    github: "https://github.com/sergekamanzi",
+    linkedin: "https://www.linkedin.com",
+    instagram: "https://www.instagram.com",
+    twitter: "https://twitter.com",
+  };
+
   return (
     <div className="main-hero">
       <h6>Hi! My name is</h6>
@@ -18,28 +27,22 @@ const Hero = () => {
         I'm a Software Developer specialized in Machine Learning and Data Science. 
         I'm also an AWS Certified Cloud Practitioner and Solutions Architect.
       </p>
-      <button className="resume-btn">
-         <IoMdDownload /> Resume
-      </button>
+      {showResume && (
+        <button className="resume-btn" onClick={() => setShowResume(false)}>
+          <IoMdDownload /> Resume
+        </button>
+      )}
       <div className="social-media">
-        <a href="https://github.com/sergekamanzi" target="_blank" rel="noopener noreferrer">
+        <a href={socialMediaLinks.github} target="_blank" rel="noopener noreferrer">
           <FaGithub />
         </a>
-        <a
-          href="https://www.linkedin.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href={socialMediaLinks.linkedin} target="_blank" rel="noopener noreferrer">
           <FaLinkedinIn />
         </a>
-        <a
-          href="https://www.instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href={socialMediaLinks.instagram} target="_blank" rel="noopener noreferrer">
           <RiInstagramFill />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <a href={socialMediaLinks.twitter} target="_blank" rel="noopener noreferrer">
           <FaXTwitter />
         </a>
       </div>
